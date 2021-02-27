0a720c7625b4b210703b241e8e90bb0d600c2d64
MHirano <michelle@hiranohomes.com>
2021-02-26 22:32:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 //Length of password
 
 int passwordLength = window.prompt("How long would you like your password to be? Enter a number between 8-128");
+
 if (passwordLength < 8)
   window.alert("Sorry please enter a length between 8 and 12");
 
